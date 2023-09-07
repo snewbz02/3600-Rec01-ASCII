e1803dec6c2b4515c3267f33d43f444ade9227ac
Sienna Newby-Sanchez <113070105+snewbz02@users.noreply.github.com>
2023-09-07 12:11:42 -0500

    Implement user input and variable declaration
    Created a program that prompts the user to enter an ASCII printable character and stores it in the 'input_char' variable. This character will be used to perform further calculations to display its decimal, hexadecimal, and binary representation.

---
+++ rec01.c
@@ -0,0 +1,24 @@
+/*
+Created by Sienna Newby-Sanchez
+
+Goal: Create a program that will prompt the user to enter
+an ASCII printable character and then print out its decimal
+(base 10), hexadecimal (base 16) and all 8 binary digits of
+its binary (base 2) representation.
+
+This will include characters such as:
+A, B, C, #, $, &, +, 0, 1, 2, <, a, b, c, and so on. 
+
+Class: 3600.004-208
+*/
+
+#include <stdio.h>
+
+int main() {
+  unsigned char input_char;
+
+  //Prompt the user to enter an ASCII character
+  printf ("Enter an ASCII character: ");
+  scanf ("%c", &input_char);
+  
+}
